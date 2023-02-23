f3b6a546a76a0db3485ece4581dcd220dc528df1
Richard Verbraak <richardverbraak@gmail.com>
2023-02-23 19:20:13 +0100

    Added about 70% of the styles and most of the HTML elements that are on the page, no functionality as of yet

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,45 +6,78 @@ const Form = () => {
       <div className="sidebar">
         <div className="sidebar__step">
           <p className="sidebar__step--number">1</p>
-          <p className="sidebar__step--sub">Step 1</p>
-          <p className="sidebar__step--title">Your Info</p>
+          <div className="sidebar__step--container">
+            <p className="sidebar__step--sub">Step 1</p>
+            <p className="sidebar__step--title">Your Info</p>
+          </div>
         </div>
 
         <div className="sidebar__step">
           <p className="sidebar__step--number">2</p>
-          <p className="sidebar__step--sub">Step 2</p>
-          <p className="sidebar__step--title">Select Plan</p>
+          <div className="sidebar__step--container">
+            <p className="sidebar__step--sub">Step 2</p>
+            <p className="sidebar__step--title">Select Plan</p>
+          </div>
+        </div>
+
+        <div className="sidebar__step">
+          <p className="sidebar__step--number">3</p>
+          <div className="sidebar__step--container">
+            <p className="sidebar__step--sub">Step 3</p>
+            <p className="sidebar__step--title">Add-ons</p>
+          </div>
+        </div>
+
+        <div className="sidebar__step">
+          <p className="sidebar__step--number">4</p>
+          <div className="sidebar__step--container">
+            <p className="sidebar__step--sub">Step 4</p>
+            <p className="sidebar__step--title">Summary</p>
+          </div>
         </div>
       </div>
 
-      <div className="sidebar__step">
-        <p className="sidebar__step--number">3</p>
-        <p className="sidebar__step--sub">Step 3</p>
-        <p className="sidebar__step--title">Add-ons</p>
-      </div>
-
-      <div className="sidebar__step">
-        <p className="sidebar__step--number">4</p>
-        <p className="sidebar__step--sub">Step 4</p>
-        <p className="sidebar__step--title">Summary</p>
-      </div>
-
       <div className="form">
-        <h1 className="form__header">Personal Info</h1>
-        <p className="form__sub">
-          Please provide your name, email address, and phone number.
-        </p>
+        <header className="form__header">
+          <h1 className="form__header--title">Personal Info</h1>
+          <p className="form__header--sub">
+            Please provide your name, email address, and phone number.
+          </p>
+        </header>
 
-        <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-          <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" id="email" />
-          <label htmlFor="phone">Phone Number</label>
-          <input type="number" name="phone" id="phone" />
+        <form className="form__inputs">
+          <label className="form__inputs--label" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="form__inputs--input"
+            type="text"
+            name="name"
+            id="name"
+          />
+
+          <label className="form__inputs--label" htmlFor="email">
+            Email Address
+          </label>
+          <input
+            className="form__inputs--input"
+            type="email"
+            name="email"
+            id="email"
+          />
+
+          <label className="form__inputs--label" htmlFor="phone">
+            Phone Number
+          </label>
+          <input
+            className="form__inputs--input"
+            type="number"
+            name="phone"
+            id="phone"
+          />
+
+          <button className="form__button">Next Step</button>
         </form>
-
-        <button>Next Step</button>
       </div>
     </main>
   );
