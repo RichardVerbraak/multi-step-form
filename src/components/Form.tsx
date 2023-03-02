60ba1a87edf067939e7594625cfbd67b28544c66
Richard Verbraak <richardverbraak@gmail.com>
2023-03-02 16:30:13 +0100

    Spacing now more accurate to the img

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -46,38 +46,43 @@ const Form = () => {
         </header>
 
         <form className="form__inputs">
-          <label className="form__inputs--label" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="form__inputs--input"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Full Name"
-          />
+          <div className="form-group">
+            <label className="form__inputs--label" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="form__inputs--input"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Full Name"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form__inputs--label" htmlFor="email">
+              Email Address
+            </label>
+            <input
+              className="form__inputs--input"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+            />
+          </div>
 
-          <label className="form__inputs--label" htmlFor="email">
-            Email Address
-          </label>
-          <input
-            className="form__inputs--input"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email Address"
-          />
-
-          <label className="form__inputs--label" htmlFor="phone">
-            Phone Number
-          </label>
-          <input
-            className="form__inputs--input"
-            type="number"
-            name="phone"
-            id="phone"
-            placeholder="e.g + 1 234 567 890"
-          />
+          <div className="form-group">
+            <label className="form__inputs--label" htmlFor="phone">
+              Phone Number
+            </label>
+            <input
+              className="form__inputs--input"
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="e.g + 1 234 567 890"
+            />
+          </div>
 
           <button className="form__button">Next Step</button>
         </form>
